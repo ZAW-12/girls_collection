4aff47d019bf74bd998d17065f232d27d6a6049c
Tun <wz25367016@ga.ttc.ac.jp>
2025-10-29 16:38:43 +0900

    Add files via upload

---
+++ products.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    name: "MaoningLV",
+    image: "MAONINGLVblack.jpg",
+    category: "shoes",
+    details: "Elegant MaoningLV available in 3 colors.",
+    colors: [
+      { name: "Black", code: "black", image: "MAONINGLVblack.jpg" },
+      { name: "White", code: "white", image: "MAONINGLVwhite.jpg" },
+      { name: "Beige", code: "beige", image: "MAONINGLVbrown.jpg" }
+    ]
+  },
+  {
+    name: "Sequin Slip Dress",
+    image: "sequin slip dress.jpg",
+    category: "signature",
+    details: "Luxury Sequin Slip evening dress in nude beige.",
+    colors: [
+      { name: "Nude Beige", code: "#d4a373", image: "sequin slip dress.jpg" }
+    ]
+  },
+  {
+    name: "Thigh Chain (Snake)",
+    image: "thigh chainsnake gold.jpg",
+    category: "queen",
+    details: "Thigh Chain available in 2 colors ",
+    colors: [
+      { name: "Gold", code: "#fed108ff", image: "thigh chainsnake gold.jpg" },
+      { name: "Silver", code:"#eae5e5ff", image: "thigh chainsnake silver.jpg"}
+    ]
+  },
+  {
+    name: "Long Sleeved Cardigan 2 pieces set",
+    image: "Long Sleeved Cardigan 2 pieces set pink.jpg",
+    category: "apparel",
+    details: "Long Sleeved Cardigan available in 3 colors ",
+    colors: [
+      { name: "Pink", code: "#f30876ff", image: "Long Sleeved Cardigan 2 pieces set pink.jpg" },
+      { name: "Black", code:"#0b0b0bff", image: "Long Sleeved Cardigan 2 pieces set black.jpg"},
+      { name: "light mocha beige", code:"#c8ab91", image: "Long Sleeved Cardigan 2 pieces set light mocha beige.jpg"}
+    ]
+  }
+
+
+];
